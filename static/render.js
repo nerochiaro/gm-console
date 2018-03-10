36965ba6721700a52268c0c92180002a2e3f7515
Ugo Riboni <nerochiaro@gmail.com>
2018-03-11 00:44:52 +0100

    Display orientation by rendering a model using ThreeJS

---
+++ static/render.js
@@ -0,0 +1,56 @@
+function Render(vue) {
+    var camera = null;
+    this.init = function(canvas, width, height) {
+      var renderer = new THREE.WebGLRenderer({
+        canvas: canvas, antialias: true, alpha: true
+      });
+      renderer.setSize(width, height);
+
+      var scene = new THREE.Scene();
+
+      camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 10000);
+      camera.position.set(150, 200, 200);
+      camera.zoom = this.zoom;
+      camera.updateProjectionMatrix();
+      scene.add(camera);
+
+      var pointLight = new THREE.PointLight(0xffffff);
+      pointLight.position.set(0, 300, 200);
+      scene.add(pointLight);
+
+      var clock = new THREE.Clock();
+      var target = null;
+      var render = function() {
+          requestAnimationFrame(render);
+          target.rotation.x = this.orient.x;
+          target.rotation.y = this.orient.y;
+          target.rotation.z = this.orient.z;
+          renderer.render(scene, camera);
+      }.bind(this);
+
+      var manager = new THREE.LoadingManager();
+      var loader = new THREE.OBJLoader(manager);
+      loader.load('human.obj', function (o) {
+          target = o;
+          o.rotation.y = Math.PI * 45 / 180;
+          scene.add(o);
+          var box = new THREE.BoxHelper( o, 0xff00 );
+          o.children[0].geometry.computeBoundingBox();
+          var bbox = o.children[0].geometry.boundingBox;
+          console.log(bbox);
+          scene.add(box);
+          camera.lookAt(bbox.center());
+          render(o);
+      }, function (xhr) {
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      }, function ( error ) {
+          console.log( 'An error happened' );
+      });
+  }.bind(vue);
+
+  this.adjustZoom = function() {
+    if (this.zoom < 0) camera.zoom = 1 / (this.zoom * -1);
+    else camera.zoom = this.zoom;
+    camera.updateProjectionMatrix();
+  }.bind(vue);
+}
