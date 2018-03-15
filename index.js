afd90b9031788b5ad331725bc1f95acaef45a5f4
Ugo Riboni <nerochiaro@gmail.com>
2018-03-15 13:11:30 +0100

    Remove the HTTP GET interface for adding data, as currently not used anymore

--- index.js
+++ index.js
@@ -14,32 +14,6 @@ var webserver = app.listen(port, function() {
 });
 var ioserver = io(webserver);
 
-var routes = express.Router();
-routes.get('/set/:player/orient/:x,:y,:z', function(req, res) {
-    res.status(200).send();
-    var msg = { player: req.params.player,
-                quat: false, w: 0.0,
-                x: parseFloat(req.params.x) || 0,
-                y: parseFloat(req.params.y) || 0,
-                z: parseFloat(req.params.z) || 0 };
-    ioserver.emit('orientation', msg);
-});
-routes.get('/set/:player/location/:lat,:lon', function(req, res) {
-    res.status(200).send();
-    var msg = { player: req.params.player,
-                lat: parseFloat(req.params.lat) || 0,
-                lon: parseFloat(req.params.lon) || 0 };
-    ioserver.emit('location', msg);
-});
-routes.get('/set/:player/mic/:mic,:interrupt', function(req, res) {
-    res.status(200).send();
-    var msg = { player: req.params.player,
-                mic: parseInt(req.params.mic) || 0,
-                interrupt: req.params.interrupt == "true" };
-    ioserver.emit('mic', msg);
-});
-app.use('/', routes);
-
 var Player = function(name) {
     this.name = name;
     this.adjust = { x: 0, y: 0, z: 0, w: 0 },
